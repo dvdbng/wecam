6db94245fd05479b3e85c0a0117ca09211b6fbe5
David Bengoa <david@bengoarocandio.com>
2012-02-01 12:01:30 +0100

    comentarios

--- js/wecamlib.js
+++ js/wecamlib.js
@@ -8,11 +8,12 @@ var wecam = {
         this.ctx = this.canvas.getContext("2d");
     },
     render: function(bytes,width,colors){
-        var s = this.size;
-        var sw = s/(width+2);
+        var s = this.size; // Canvas width
+        var sw = s/(width+2); // Square width
         var ctx = this.ctx;
         var color = false;
 
+        // Draw border
         for(var i=0; i<width+2; ++i){
             ctx.fillStyle = color?"black":"white";
             color = !color;
@@ -22,6 +23,7 @@ var wecam = {
             ctx.fillRect(i*sw,s-sw,sw,sw);
             ctx.fillRect(s-sw,i*sw,sw,sw);
         }
+        // Draw center
         for(var i=0, l=width*width; i<l; ++i){
             ctx.fillStyle = "rgb(" + bytes[i*3] + "," + bytes[i*3+1] + "," + bytes[i*3+2] + ")";
             ctx.fillRect(
@@ -34,7 +36,7 @@ var wecam = {
     }
 };
 
-var cd = 2; // Numero de bits por canal
+var cd = 1; // Numero de bits por canal
 wecam.init();
 var aob = [255,255,255];
 for(i=0; i<128*128*3; ++i){
